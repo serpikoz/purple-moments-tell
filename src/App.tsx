4824e935ed3c03a6130b39e12f6306cc3a0c4567
Kadir <116947551+serpikoz@users.noreply.github.com>
2024-01-28 19:16:05 +0300

    continue

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { GitHubBanner, Refine, WelcomePage } from "@refinedev/core";
+import { Authenticated, Refine } from "@refinedev/core";
 import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
 import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
 
@@ -7,18 +7,19 @@ import "@refinedev/antd/dist/reset.css";
 
 import { authProvider, dataProvider, liveProvider } from "./providers";
 import routerBindings, {
+  CatchAllNavigate,
   DocumentTitleHandler,
   UnsavedChangesNotifier,
 } from "@refinedev/react-router-v6";
 import { App as AntdApp } from "antd";
 import { Home, ForgotPassword, Login, Register } from "./pages";
 
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
+import { Layout } from "./components";
 
 function App() {
   return (
     <BrowserRouter>
-      <GitHubBanner />
       <RefineKbarProvider>
         <AntdApp>
           <DevtoolsProvider>
@@ -37,12 +38,26 @@ function App() {
               }}
             >
               <Routes>
-                <Route index element={<WelcomePage />} />
                 <Route index element={<Home />} />
                 <Route path="/register" element={<Register />} />
-                <Route path="login" element={<Login />} />
-                <Route path="forgot-password" element={<ForgotPassword />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/forgot-password" element={<ForgotPassword />} />
+                <Route
+                  element={
+                    <Authenticated
+                      key="authenticated-layout"
+                      fallback={<CatchAllNavigate to="/login" />}
+                    >
+                      <Layout>
+                        <Outlet />
+                      </Layout>
+                    </Authenticated>
+                  }
+                >
+                  <Route index element={<Home />} />
+                </Route>
               </Routes>
+
               <RefineKbar />
               <UnsavedChangesNotifier />
               <DocumentTitleHandler />
---
+++ src/components/layout/index.tsx
@@ -0,0 +1,17 @@
+import { ThemedLayoutV2, ThemedTitleV2 } from "@refinedev/antd";
+import Header from "./header";
+
+export const Layout = ({ children }: React.PropsWithChildren) => {
+  return (
+    <>
+      <ThemedLayoutV2
+        Header={Header}
+        Title={(titleProps) => {
+          return <ThemedTitleV2 {...titleProps} text="Refine" />;
+        }}
+      >
+        {children}
+      </ThemedLayoutV2>
+    </>
+  );
+};
